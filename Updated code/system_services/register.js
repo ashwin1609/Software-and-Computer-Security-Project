8933d9ce7c0320356dc6c72140115b1e13f5c3f7
mihirkumar1212 <pmihir2609@gmail.com>
2022-03-31 19:32:01 -0400

    authentication and register files updated

---
+++ Updated code/system_services/register.js
@@ -0,0 +1,73 @@
+const AWS = require('aws-sdk')
+AWS.config.update({
+    region: 'us-east-1'
+})
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+const userTable = 'UserInfo';
+const util = require('../utils/util')
+const bcrypt = require('bcryptjs');
+
+async function register(UserInfo){
+    const name = UserInfo.name;
+    const email = UserInfo.email;
+    const username = UserInfo.username;
+    const password = UserInfo.password;
+
+    if(!username || !name || !email || !password){
+        return util.buildResponse(401, {
+            message: 'All inputs fields are required!!'
+        })
+    }
+
+    const dynamoUser = await getUser(username);
+    if(dynamoUser && dynamoUser.username){
+        return util.buildResponse(401, {
+            message: 'The following username exists in our database '
+        })
+    }
+
+    const encryptedPassword = bcrypt.hashSync(password.trim(), 10);
+    const user = {
+        name: name,
+        email: email,
+        username: username.toLowerCase().trim(),
+        password: encryptedPassword
+    }
+
+    const storeUserData = await saveUser(user);
+    if(!storeUserData){
+        return util.buildResponse(503, {
+            message: 'Server Error. Please try again later'
+        })
+    }
+    return util.buildResponse(200, { username: username});
+}
+
+async function getUser(username) {
+    const param = {
+        TableName: userTable,
+        Key: {
+            username: username
+        }
+    }
+
+    return await dynamodb.get(params).promise().then(response => {
+        return response.Item;
+    }, error => {
+        console.error("There is an error:", error);
+    })
+}
+
+async function saveUser(user){
+    const params = {
+        TableName: userTable,
+        Item: user
+    }
+    return await dynamodb.put(params).promise().then(() => {
+        return true;
+    }, error => {
+        console.error("There is an error:", error);
+    })
+}
+
+module.exports.register = register;
---
+++ Updated code/utils/auth.js
@@ -0,0 +1,41 @@
+// using json webtoken
+const jwt = require('jsonwebtoken');
+
+function generateToken(UserInfo) {
+    if(!user){
+        return null;
+
+    }
+
+    // you can also hardcode the secret key if you want at 33.36
+    return jwt.sign(UserInfo, process.env.JWT_SECRET, {
+        expiresIN: '1h'
+    })
+}
+
+    function verifyToken(username, toeken) {
+        return jwt.verify(token, process.env.JWT_SECRET, (error, response) =>{
+            if (error){
+                return {
+                    verified: false,
+                    message: 'invalid token'
+                }
+            }
+
+            if(response.username !== username){
+                return {
+                    verified: false,
+                    message: 'invalid user'
+                } 
+            }
+
+            return {
+                verified: true,
+                message: 'verified' 
+            }
+        } )
+    }
+
+
+module.exports.generateToken = generateToken;
+module.exports.verifyToeken = verifyToken;
